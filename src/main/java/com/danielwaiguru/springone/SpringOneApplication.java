427a5f6ff2a9e2f2fddde0ba4a4b7fe867a71c6a
Daniel Waiguru <danielirungu91@gmail.com>
2023-11-24 07:58:25 +0300

    initial commit

---
+++ src/main/java/com/danielwaiguru/springone/SpringOneApplication.java
@@ -0,0 +1,13 @@
+package com.danielwaiguru.springone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOneApplication.class, args);
+    }
+
+}
